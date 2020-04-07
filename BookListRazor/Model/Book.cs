6e1128f39537aa9955f9bf9c2b6d06fe570cb39a
Rushikesh Musale <rushikeshmusale@gmail.com>
2020-04-07 20:24:10 +0530

    Added a book Model to database Added required packagees:   ○ Microsoft.EntityFramework.Core   ○ Microsoft.EntityFrameworkCore.SqlServer // to register the application db context in startup.configure() Microsoft.EntityFrameworkCore.Tools // to run migrations
    Performed first migration

---
+++ BookListRazor/Model/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Model
+{
+    public class Book
+    {
+        [Key] // to tell entity framework that it is auto generated property
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Author { get; set; }
+    }
+}
--- BookListRazor/Startup.cs
+++ BookListRazor/Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookListRazor.Model;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,7 +25,12 @@ namespace BookListRazor
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            
+            services
+                .AddDbContext<ApplicationDbContext>(
+                    option => option
+                                .UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))  // Name given in appsetting.json
+                );
+
             services
                 .AddRazorPages()
                 .AddRazorRuntimeCompilation(); // so that we can edit view while running the application
